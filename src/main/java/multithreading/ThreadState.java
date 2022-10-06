71d64fcd8b505378522c10a1824a33515836d1cc
max <m.asadi@gmail.com>
2022-10-06 14:06:07 +0330

    Implementing States of threads

--- src/main/java/multithreading/ThreadState.java
+++ src/main/java/multithreading/ThreadState.java
@@ -2,11 +2,13 @@ package multithreading;
 
 public class ThreadState {
     public static void main(String[] args) throws InterruptedException {
+
         Thread thread = new Thread(new ThreadJoin());
         System.out.println("ThreadJoin state is "+ thread.getState());
         thread.start();
         System.out.println("ThreadJoin state is "+ thread.getState());
         thread.join(2000);
         System.out.println("ThreadJoin state is "+ thread.getState());
+
     }
 }
