614cf954dd48cd38a693ff2ee34413efbe5e9dd5
max <m.asadi@gmail.com>
2022-10-06 12:23:58 +0330

    Initializing java exercises project

---
+++ src/test/java/study/practice/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.practice.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
